70ed3b2955b532832d9a42c20c603ff869864d8b
Florian Bruggisser <cansik@users.noreply.github.com>
2021-02-18 14:12:32 +0100

    implemented scripts for manager service

---
+++ manager-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### This scripts starts the manager and can be used for testsing or deployment.
+### The manager iteself is configured by relative pahts, so it's important that
+### it runs inside the ./manager directory.
+### Florian Bruggisser 18.02.2021
+
+# Read the asolute path to the script file.
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "Working Directory: $SCRIPT_PATH"
+
+# switch to working directory
+pushd "$SCRIPT_PATH/manager/"
+
+# setup npm
+npm install
+
+# run bus manager
+node busManager.js
+
+popd
---
+++ manager-service-install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+### Installs the manager as a service and all its dependencies.
+### Florian Bruggisser 18.02.2021
+
+# Read the asolute path to the script file.
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+pushd "$SCRIPT_PATH"
+
+# load configuration
+source manager-service.cfg
+
+# install prerequisites if necessary (only on Linux)
+echo "installing msoquitto..."
+sudo apt update
+sudo apt install acl -y
+
+# add user
+echo "adding service user..."
+useradd -s /usr/sbin/nologin -r -M $SERVICE_USER_NAME
+
+# grant access
+setfacl -R -m u:$SERVICE_USER_NAME:rwx "$SCRIPT_PATH"
+
+# prepare service configuration form template
+echo "installing service..."
+RUN_SCRIPT_PATH="$SCRIPT_PATH/broker-run.sh"
+cp "broker/$SERVICE_NAME.service.txt" "$SERVICE_NAME.service"
+
+sed -i -e "s~%COMMAND%~$RUN_SCRIPT_PATH~g" "$SERVICE_NAME.service"
+sed -i -e "s~%USER%~$SERVICE_USER_NAME~g" "$SERVICE_NAME.service"
+
+# install as service
+sudo mv $SERVICE_NAME.service "/etc/systemd/system/$SERVICE_NAME.service"
+
+echo "enabling service..."
+sudo systemctl enable $SERVICE_NAME.service
+
+echo ""
+echo ""
+echo "Please start your service now:"
+echo "sudo systemctl start $SERVICE_NAME.service"
+echo ""
+echo ""
+
+popd
